a0a1b654e11c2655eb557fdc99cca0fba62aac14
maxence468 <maxencebarth12@gmail.com>
2025-12-11 13:20:41 +0100

    factory personne

--- app/Models/Personne.php
+++ app/Models/Personne.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Personne extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['nomPers','prePers','dateNaissPers','lieuNaissPers','photoPers','biblio'];
     protected $table = 'personnes';
     protected $primaryKey = 'idPers';
