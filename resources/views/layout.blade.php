fc539d65e0d8b6e99f8e2d7789fc46f692a37454
DylanG-Dev <gametdylan@gmail.com>
2026-02-15 14:54:45 +0100

    Maj CSS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,9 +29,10 @@
                     <a href="/rechercheActeur" class="nav-text">Recherche acteur</a>
                     <a href="/rechercheGenre" class="nav-text">Recherche genre</a>
                     <a href="/progSemaineCinema" class="nav-text">Programme de la semaine</a>
+                    <a href="/gestionFilm" class="nav-text">Gestion catalogue</a>
                     @auth
                         @if(Auth::user()->isAdmin())
-                            <a href="/gestionCatalogue" class="nav-text">Gestion catalogue</a>
+                            <a href="/gestionFilm" class="nav-text">Gestion catalogue</a>
                         @else
                             <a href="" class="nav-text">Réservation</a>
                             <a href="/parametreUtilisateur/{{ Auth::id() }}/edit" class="nav-text">Paramètres</a>
