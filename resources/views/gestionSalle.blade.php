ea00e05ae30d18433b953d04ca05d8afb60c2144
DylanG-Dev <gametdylan@gmail.com>
2026-03-05 17:00:40 +0100

    MAJ de la page 'gestionSalle' pour Maxence

--- resources/views/gestionSalle.blade.php
+++ resources/views/gestionSalle.blade.php
@@ -38,12 +38,13 @@
 
                         <div class="row align-items-center mb-4 pt-3">
                             <div class="col-12 col-lg-3 order-4 order-lg-3">
-                                <label class="h3 mb-0">Capacité salle</label>
+                                <label class="h3 mb-0">Cinéma</label>
                             </div>
 
                             <div class="col-12 col-lg-4 order-5 order-lg-4">
-                                <input class="inputCatalogue" type="number" min="1" max="1000" step="1"
-                                       title="La capacité de la salle doit être entre 1 et 1000" placeholder="Capacité salle" required>
+                                <select name="cinema" class="choixCatal" required>
+                                    <option value="">Cinéma</option>
+                                </select>
                             </div>
 
                             <div class="col-12 col-lg-5 order-3 order-lg-5 d-lg-flex justify-content-center pt-2 pt-lg-0">
@@ -61,7 +62,7 @@
                             </div>
                             <div class="col-12 col-lg-4">
                                 <select name="typeSalle" class="choixCatal" required>
-                                    <option value=""></option>
+                                    <option value="">Type de salle</option>
                                 </select>
                             </div>
                             <div class="col-lg-5 d-none d-lg-block"></div>
@@ -69,17 +70,42 @@
 
                         <div class="row align-items-center mb-4">
                             <div class="col-12 col-lg-3">
-                                <label class="h3 mb-0">Cinéma</label>
+                                <label class="h3 mb-0">Numéro de salle</label>
                             </div>
                             <div class="col-12 col-lg-4">
-                                <select name="cinema" class="choixCatal" required>
-                                    <option value=""></option>
-                                </select>
+                                <input class="inputCatalogue" type="number" min="1" max="1000" step="1"
+                                       title="La numéro de la salle doit être entre 1 et 1000" placeholder="Numéro de salle" required>
                             </div>
                             <div class="col-lg-5 d-none d-lg-block"></div>
                         </div>
 
+                        <div class="row align-items-center mb-4">
+                            <div class="col-12 col-lg-3">
+                                <label class="h3 mb-0">Capacité salle</label>
+                            </div>
+                            <div class="col-12 col-lg-4">
+                                <input class="inputCatalogue" type="number" min="1" max="1000" step="1"
+                                       title="La capacité de la salle doit être entre 1 et 1000" placeholder="Capacité salle" required>
+                            </div>
+                            <div class="col-lg-5 d-none d-lg-block"></div>
+                        </div>
+
+                        <div class="row align-items-center mb-4">
+                            <div class="col-12 col-lg-3">
+                                <label class="h3 mb-0">Tarif</label>
+                            </div>
+                            <div class="col-12 col-lg-4">
+                                <select name="tarif" class="choixCatal" required>
+                                    <option value="">Tarif</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-5 d-none d-lg-block"></div>
+                        </div>
+                        <div>
+
+                        </div>
                     </form>
+
                 </div>
             </div>
         </div>
@@ -89,5 +115,47 @@
             <button form="myForm" name="btnModif" class="btn-ajoutModifSuppr" type="submit"><span>Modifier</span></button>
             <button form="myForm" name="btnSuppr" class="btn-ajoutModifSuppr" type="submit"><span>Supprimer</span></button>
         </div>
+        <template id="tplFormTarif">
+            <form id="myForm" method="post" action="">
+                @csrf
+                <div class="row align-items-center mb-1">
+                    <div class="col-12 col-lg-7 order-1 pt-4 pb-4">
+                        <h3 class="mb-3">Ajouter, modifier ou supprimer un tarif</h3>
+                    </div>
+                    <div class="col-12 col-lg-5 order-2 d-lg-flex justify-content-center">
+                        <div class="alignment-wrapper">
+                            <label class="h3 mb-3 labelFilm pt-5" for="listeFilm">Tarif à modifier</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row align-items-center mb-4">
+                    <div class="col-12 col-lg-3 order-4 order-lg-3">
+                        <label class="h3 mb-0">Libelle tarif</label>
+                    </div>
+
+                    <div class="col-12 col-lg-4 order-5 order-lg-4">
+                        <input class="inputCatalogue" type="text" placeholder="Libelle tarif" required>
+                    </div>
+
+                    <div class="col-12 col-lg-5 order-3 order-lg-5 d-lg-flex justify-content-center pt-2 pt-lg-0">
+                        <div class="alignment-wrapper">
+                            <select name="movie" class="choixCatal" onchange="this.form.submit()">
+                                <option value=""></option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row align-items-center mb-4">
+                    <div class="col-12 col-lg-3">
+                        <label class="h3 mb-0">Prix tarif</label>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <input class="inputCatalogue" type="text" placeholder="Prix tarif" required>
+                    </div>
+                </div>
+            </form>
+        </template>
     </main>
 @endsection
