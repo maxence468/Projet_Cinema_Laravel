933013ca252bc27fd819659b44a66ce4afdd0a76
maxence468 <maxencebarth12@gmail.com>
2026-03-09 10:30:20 +0100

    doublon seanceController

--- app/Http/Controllers/SeanceController.php
+++ app/Http/Controllers/SeanceController.php
@@ -9,19 +9,24 @@ use Illuminate\Http\Request;
 
 class SeanceController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $seances = Seance::all();
         return view('seances.index', compact('seances'));
     }
-    public function show(Seance $seance){
+
+    public function show(Seance $seance)
+    {
         return view('seances.show', compact('seance'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('seances.create');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         request()->validate([
             'heureSeance' => 'required|date_format:H:i',
             'dateSeance' => 'required|date',
@@ -41,19 +46,20 @@ class SeanceController extends Controller
         return redirect()->route('seances.index');
     }
 
-    public function edit(Seance $seance){
+    public function edit(Seance $seance)
+    {
         return view('seances.edit', compact('seance'));
     }
 
     public function update(Request $request, $id)
     {
-            request()->validate([
-                'heureSeance' => 'required|date_format:H:i',
-                'dateSeance' => 'required|date',
-                'dureeSeance' => 'required|integer',
-                'idFilm' => 'required|exists:films,idFilm',
-                'idSalle' => 'required|exists:salles,idSalle',
-            ]);
+        request()->validate([
+            'heureSeance' => 'required|date_format:H:i',
+            'dateSeance' => 'required|date',
+            'dureeSeance' => 'required|integer',
+            'idFilm' => 'required|exists:films,idFilm',
+            'idSalle' => 'required|exists:salles,idSalle',
+        ]);
 
         $seance = Seance::findOrFail($id);
 
@@ -81,56 +87,13 @@ class SeanceController extends Controller
         ]);
     }
 
-    public function editSeance(Request $request){
+    public function editSeance(Request $request)
+    {
         $id = $request->idSeance;
         $seance = Seance::find($id);
 
         return response()->json([
-            'seance'=> $seance,
+            'seance' => $seance,
         ]);
-    }
-
-    public function create(){
-        $salles = Salle::all();
-        $films = Film::all();
-        return view('seances.create',compact('salles','films'));
-    }
-
-    public function store(Request $request){
-        $validated = $request->validate([
-            'dateSeance' => 'required',
-            'heureSeance' => 'required',
-            'idSalle' => 'required',
-            'idFilm' => 'required',
-            'dureeSeance' => 'required',
-        ]);
-
-        $s = new Seance();
-        $s->dateSeance = $validated['dateSeance'];
-        $s->heureSeance = $validated['heureSeance'];
-        $s->idSalle = $validated['idSalle'];
-        $s->idFilm = $validated['idFilm'];
-        $s->dureeSeance = $validated['dureeSeance'];
-        $s->save();
-
-        return redirect('/seances/'.$s->idSeance);
-
-    }
-
-    public function edit(Seance $seance){
-        $salles = Salle::all();
-        $films = Film::all();
-        return view('seances.edit', compact('seance','salles','films'));
-    }
-
-    public function update(Request $request, Seance $seance){
-        $date = $request->only(['dateSeance','heureSeance','idSalle','idFilm','dureeSeance']);
-        $seance->update(array_filter($date));
-        return redirect('/seances'.$seance->idSeance);
-    }
-
-    public function destroy(Seance $seance){
-        $seance->delete();
-        return redirect('/seances');
     }
 }
