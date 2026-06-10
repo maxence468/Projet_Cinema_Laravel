1511a9d5108f7e7207c50cfb9fb88c5a73e32beb
DylanG-Dev <gametdylan@gmail.com>
2026-06-10 09:25:38 +0200

    Initialisation

---
+++ resources/views/components/btnAddEditDeleteManagementForm.blade.php
@@ -0,0 +1,5 @@
+<div class="d-flex flex-wrap justify-content-center justify-content-lg-end pt-5 pb-5">
+    <button name="btnAjout" class="btn-ajoutModifSuppr" id="btnAjt"><span>Ajouter</span></button>
+    <button name="btnModif" class="btn-ajoutModifSuppr" id="btnModif"><span>Modifier</span></button>
+    <button name="btnSuppr" class="btn-ajoutModifSuppr" id="btnSuppr"><span>Supprimer</span></button>
+</div>
